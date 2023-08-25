4781cc7bb09841466914b8ee3c3aa6ae4590d3a0
AntonioMesquit <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-25 14:07:06 -0300

    up

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const abrir = document.querySelector(".list1");
 const abrir2 = document.querySelector(".list2")
-const menu = document.querySelector(".nav-list")
-const burguer = document.querySelector(".burguer")
+const featuresMobile = document.querySelector(".features-mobile")
+const burguer = document.querySelector(".burguer1")
 const sidebar = document.querySelector(".sidebar");
+const company = document.querySelector(".company-mobile")
+
+
+
+
 abrir.addEventListener('click' , () => {
  
     abrir.classList.toggle("active")
@@ -12,11 +17,15 @@ abrir2.addEventListener('click' , () => {
     abrir2.classList.toggle("active")
 })
 
-menu.addEventListener('click' , () => {
-    menu.classList.toggle("active");
-    
-})
+
 burguer.addEventListener('click' , () => {
     burguer.classList.toggle("active");
     sidebar.classList.toggle("active");
+  
 })
+featuresMobile.addEventListener('click' , () => {
+    featuresMobile.classList.toggle("active")
+ })
+ company.addEventListener('click' , () => {
+    company.classList.toggle("active")
+ })
